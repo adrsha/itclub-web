1df60f7b36405c79e0b48ccb7d1dab049980289f
asur-zx <asur-zx@github.com>
2024-12-30 11:34:28 +0545

    dev changes

--- src/Lines/Lines.jsx
+++ src/Lines/Lines.jsx
@@ -6,27 +6,28 @@ import { lenis } from "../Lenis/Lenis";
 function Lines() {
   const [scrollY, setScrollY] = useState(0);
   const rafIdRef = useRef(null);
-  const heightRef = useRef(0);
+  const heightRef = useRef(0); //reference to the root height
   const innerHeightRef = useRef(0);
   const bulbsRef = useRef([]);
 
   useEffect(() => {
-    heightRef.current = document.documentElement.getBoundingClientRect().height;
-    innerHeightRef.current = window.innerHeight;
+    heightRef.current = document.documentElement.getBoundingClientRect().height;//bounding box of the root(may not be visible)
+    innerHeightRef.current = window.innerHeight; //height of viewport, can change due to resize
     bulbsRef.current = document.querySelectorAll(".bulb");
   }, []);
 
   const calculateBulbPosition = useCallback((scroll) => {
     return (
       scroll +
-      (scroll / (heightRef.current - innerHeightRef.current + 300)) *
+      (scroll / (heightRef.current - innerHeightRef.current+1)) *
         innerHeightRef.current
+       //[scroll / (heightRef.current - innerHeightRef.current + 300)) *innerHeightRef.current] gives how much content can be scrolled that is not visible
     );
   }, []);
 
   const handleScroll = useCallback(
     throttle(() => {
-      setScrollY(calculateBulbPosition(lenis.scroll));
+      setScrollY(calculateBulbPosition(lenis.scroll)); //lenis scroll returns current vert scroll position of page
     }, 16),
     [calculateBulbPosition],
   );
