6056fdef42c5dd0a7bb4a2ad8594d670a3c93df3
adrsha <adrsha@github.com>
2024-12-30 20:41:04 +0545

    [Added] form links to Dristi Cards

--- src/Dristi/Dristi.jsx
+++ src/Dristi/Dristi.jsx
@@ -12,13 +12,14 @@ import Parallax from '../Parallax/Parallax.js';
 function DristiEvents(props) {
   const contentRef = useRef(null);
 
+    console.log(props)
   return (
     <>
       {isMobile() ? null : <Parallax />}
       <div
         className="specialEventContent parallaxEl"
         ref={contentRef}
-          data-lerp={props.totalCols % 2 != 0 ? (props.column == Math.ceil(props.totalCols/2) ? (props.column * 10):-15) : 5}
+          data-lerp={props.totalCols % 2 != 0 ? (props.column == Math.ceil(props.totalCols/2) ? (20): 5) : 5}
       >
         {props.posterpath ? (
           <img className="specialEventContentImg" src={props.posterpath} />
@@ -28,6 +29,9 @@ function DristiEvents(props) {
         <div className="specialEventContentTitle">{props.name}</div>
         <div className="specialEventContentTime">{props.time}</div>
         <div className="specialEventsDesc">{props.desc}</div>
+        <a href={props.formLinks}>
+          <button className="specialEventButton">Register</button>
+        </a>
       </div>
     </>
   );
@@ -43,36 +47,49 @@ function EventsList() {
       time: ' 6-7 hours event, Day 2',
       desc: 'UI/UX Design Competition challenges participants to create user-friendly and visually appealing interfaces.',
       posterpath: '/UI_UX_poster.png',
+      formLinks: 'https://forms.gle/QtPWLMCMN4utRfQd6'
     },
     {
       name: 'Valorant Tournament',
       time: 'Day 1-3',
       desc: 'Valorant Esports Tournament is a competitive gaming event where participants battle in the popular tactical shooter game, Valorant. Players showcase their strategic thinking, teamwork, and gaming skills in high-energy matches.',
       posterpath: '/valo_poster.png',
+      formLinks: 'https://forms.gle/kAJgrJKzo7pv9g8MA'
     },
     {
       name: 'PUBG Tournament',
       time: 'Day 2',
       desc: 'PUBG Esports Tournament is a competitive gaming event where players compete in the intense battle royale game, PlayerUnknown\'s Battlegrounds (PUBG). Contestants demonstrate their tactical thinking, survival instincts, teamwork, and combat skills in high-stakes matches, vying for victory in this fast-paced and strategic environment',
       posterpath: '/pubg_poster.png',
+      formLinks: 'https://forms.gle/cZGutgnAXSChxQCMA'
     },
     {
       name: 'Hackathon',
       time: '48 hours event, Day 1 - Day 2',
       desc: 'The Hackathon at Dristi 3.0 is a 48-hour event where teams will collaborate to develop innovative solutions to real-world challenges. Participants will work under time constraints, tackling problems related to AI, while receiving guidance from mentors.',
       posterpath: '/defaultPoster.png',
+      formLinks: '#'
     },
     {
       name: 'Quick Code',
       time: 'Everyday upto 50 Questions',
       desc: 'Quick Code and Win is a fast-paced competition where participants solve coding problems in real-time.',
       posterpath: '/defaultPoster.png',
+      formLinks: '#'
     },
     {
       name: 'Competitive Coding',
       time: '7-8 hours event, Day 2',
       desc: 'Competitive Coding is a contest where participants solve complex coding problems within strict time limits. This event tests their problem-solving abilities, coding efficiency, and logical thinking.',
       posterpath: '/defaultPoster.png',
+      formLinks: 'https://forms.gle/V2zUpcCjAJc3KFNYA'
+    },
+    {
+      name: 'Software Exhibition',
+      time: 'Everyday',
+      desc: 'The Software Exhibition is a platform where participants showcase innovative software projects, demonstrating their technical skills and creativity. This event highlights the latest advancements in software development and provides a space for learning and inspiration.',
+      posterpath: '/defaultPoster.png',
+      formLinks: '#'
     },
   ];
 
@@ -115,6 +132,7 @@ function EventsList() {
               posterpath={event.posterpath}
               column={columnIndex + 1}
               totalCols={columns.length}
+              formLinks={event.formLinks}
             />
           ))}
         </div>
