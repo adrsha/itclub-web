dab5552af892b58e01406a3d65b8385397f6d02a
adrsha <adrsha@github.com>
2024-12-30 17:15:41 +0545

    [Added] carousel for home page notices

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+
+import React, { useState, useEffect } from 'react';
 import Countdown from './countdown/Countdown.jsx';
 import Nav from './Nav/Nav.jsx';
 import ContactUs from './ContactUs/ContactUs.jsx';
@@ -13,46 +15,67 @@ import notices from '../data/Notices.json';
 import Lines from './Lines/Lines.jsx';
 import { LenisComponent } from './Lenis/Lenis.js';
 
+
+const Carousel = (props) => {
+    let path = props.defPath;
+    console.log(props.id)
+    if (path == "" ){
+        path = "/defaultPoster.png"
+    }
+    return <Cards
+                id="image"
+                src={path}
+            />
+};
+
 function App() {
-  return (
-    <>
-      <Nav events contactUs aboutUs notices dristi />
-      <Lines />
-      <LenisComponent />
-      {isMobile() ? null : <Parallax />}
-      <Countdown />
-      <div className="pageContent" id="homePageContent">
-        <Cards
-          id="circleButtons"
-          title="KEC IT Club"
-          button1="About Us"
-          link1="#aboutUs"
-          button2="Contact Us"
-          link2="#contactUs"
-        />
-        <Cards
-          id="image"
-          src={
-            notices[0].posterImg ? notices[0].posterImg : '/defaultPoster.png'
-          }
-        />
-        <Cards
-          id="notice"
-          title={notices[0].noticeHead}
-          content={notices[0].noticeBody}
-          button1="All Notices"
-          buttonDiscord={notices[0].buttonDiscord}
-          buttonForm={notices[0].buttonForm}
-          link1="/notices"
-          button2="All Events"
-          link2="/events"
-        />
-      </div>
-      <AboutUs />
-      <ContactUs />
-      <Footer />
-    </>
-  );
+    const [noticeVal, setNoticeVal] = useState(0);
+    useEffect(() => {
+       const interval = setInterval(() => {
+            setNoticeVal((prevVal)=>{
+                return (prevVal+1) % notices.length
+            })
+        }, 3000);
+
+        //Clearing the interval
+        return () => clearInterval(interval);
+    }, [noticeVal])
+
+
+    return (
+        <>
+            <Nav events contactUs aboutUs notices dristi />
+            <Lines />
+            <LenisComponent />
+            {isMobile() ? null : <Parallax />}
+            <Countdown />
+            <div className="pageContent" id="homePageContent">
+                <Cards
+                    id="circleButtons"
+                    title="KEC IT Club"
+                    button1="About Us"
+                    link1="#aboutUs"
+                    button2="Contact Us"
+                    link2="#contactUs"
+                />
+                <Carousel defPath={notices[noticeVal].posterImage} id={noticeVal}/>
+                <Cards
+                    id="notice"
+                    title={notices[0].noticeHead}
+                    content={notices[0].noticeBody}
+                    button1="All Notices"
+                    buttonDiscord={notices[0].buttonDiscord}
+                    buttonForm={notices[0].buttonForm}
+                    link1="/notices"
+                    button2="All Events"
+                    link2="/events"
+                />
+            </div>
+            <AboutUs />
+            <ContactUs />
+            <Footer />
+        </>
+    );
 }
 
 export default App;
