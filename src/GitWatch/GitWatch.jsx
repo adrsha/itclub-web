421d6d9861d428e57816401972a368d6a4b33505
adrsha <adrsha@github.com>
2025-01-05 11:20:09 +0545

    [Added] function in gitwatch.jsx

---
+++ src/GitWatch/GitWatch.jsx
@@ -0,0 +1,23 @@
+
+// import React, { useState, useEffect } from 'react';
+// import Countdown from './countdown/Countdown.jsx';
+// import isMobile from './ExtraFuncs.js';
+// import Parallax from './Parallax/Parallax.js';
+// import Cards from './Cards/Cards.jsx';
+// import notices from '../data/Notices.json';
+// import Lines from './Lines/Lines.jsx';
+// import { LenisComponent } from './Lenis/Lenis.js';
+
+
+// https://api.github.com/repos/ShresthaAnkit/InternTasks/commits
+function GitWatch() {
+
+  // let projectHtml = projectData.map((pr) => {})
+  return(
+    <>
+
+    </>
+  )
+}
+
+export default GitWatch();
